acf7e608591c39b9492c6dc2124faca8173dff45
Kyler Howard <kylerhoward@gmail.com>
2026-02-17 11:55:02 -0700

    Added a dynamic voltage plotter

---
+++ Utility_Functions/Plot_Voltages.m
@@ -0,0 +1,138 @@
+
+% data should contain whatever you need to plot:
+%   data.X{1}, data.Y{1}, data.Z{1}, data.C{1}, etc.
+%   If flag.do_complex = true, also include {2}
+
+%% --- Create UIFigure ---
+fig = uifigure('Position',[100 100 700 700]);
+
+%% --- Grid Layout ---
+% Layout:
+%   Row 1: plots (1 or 2)
+%   Row 2: sliders (3 sliders stacked)
+mainGrid = uigridlayout(fig,[2 1]);
+mainGrid.RowHeight = {'1x', 'fit'};
+mainGrid.ColumnWidth = {'1x'};
+
+%% --- Plot Panel ---
+if flags.set_complex
+    plotGrid = uigridlayout(mainGrid,[1 2]);
+    plotGrid.ColumnWidth = {'1x','1x'};
+else
+    plotGrid = uigridlayout(mainGrid,[1 1]);
+end
+
+%% --- Axes Creation ---
+if flags.set_complex
+    ax1 = uiaxes(plotGrid);
+    ax2 = uiaxes(plotGrid);
+else
+    ax1 = uiaxes(plotGrid);
+    ax2 = [];
+end
+
+%% --- Initial Plotting ---
+% Frame index starts at 1
+frame = 1;
+
+% Plot 1
+s1 = scatter3(ax1, nodes(:,1), nodes(:,2), nodes(:,3), 10, real(Uall(:,flags.plot_volts,frame)), 'filled');
+title(ax1, sprintf("Frame %d\nReal Voltage: CP %d", frame, flags.plot_volts))
+xlabel(ax1, "x (mm)")
+ylabel(ax1, "y (mm)")
+zlabel(ax1, "z (mm)")
+axis(ax1, 'equal')
+c = colorbar(ax1);
+c.Label.String = "mV";
+% c.Location     = 'southoutside';
+
+% Plot 2 (if complex)
+if flags.set_complex
+    s2 = scatter3(ax2, nodes(:,1), nodes(:,2), nodes(:,3), 10, imag(Uall(:,flags.plot_volts,frame)), 'filled');
+    title(ax1, sprintf("Frame %d\nImaginary Voltage: CP %d", frame, flags.plot_volts))
+    xlabel(ax1, "x (mm)")
+    ylabel(ax1, "y (mm)")
+    zlabel(ax1, "z (mm)")
+    axis(ax2, 'equal')
+    c = colorbar(ax2);
+    c.Label.String = "mV";
+    % c.Location     = 'southoutside';
+else
+    s2 = [];
+end
+
+% --- Slider Panel ---
+sliderGrid = uigridlayout(mainGrid,[3 1]);
+sliderGrid.RowHeight = {75,70,70};
+
+% --- Frame Slider Block --- 
+frameBlock = uigridlayout(sliderGrid,[2 1]); 
+frameBlock.RowHeight = {15,30};
+
+uilabel(frameBlock, ... 
+        'Text','Frame Index', ... 
+        'HorizontalAlignment','center', ... 
+        'FontWeight','bold');
+uislider(frameBlock, ...
+         'Limits',[1 size(Uall,3)], ...
+         'Value',1, ...
+         'MajorTicks',1:size(Uall,3), ...
+         'MinorTicks',[],...
+         'ValueChangedFcn',@(src,evt) updateFrame(ax1, ax2, s1, s2, src.Value, Uall, flags));
+
+% --- X-axis Slider Block --- 
+xaxisBlock = uigridlayout(sliderGrid,[2 1]); 
+xaxisBlock.RowHeight = {15,30};
+
+uilabel(xaxisBlock, ... 
+        'Text','X-Axis Zoom', ... 
+        'HorizontalAlignment','center', ... 
+        'FontWeight','bold');
+uislider(xaxisBlock, 'range',...
+         'Limits',[min(nodes(:,1)), ceil(max(nodes(:,1)))],...
+         'Value', [min(nodes(:,1)), ceil(max(nodes(:,1)))],...
+         'Step', 5,...
+         'ValueChangedFcn',@(src,evt) updateX(ax1, ax2, src.Value, flags));
+
+% --- Y-axis Slider Block --- 
+yaxisBlock = uigridlayout(sliderGrid,[2 1]); 
+yaxisBlock.RowHeight = {15,30};
+
+uilabel(yaxisBlock, ... 
+        'Text','Y-Axis Zoom', ... 
+        'HorizontalAlignment','center', ... 
+        'FontWeight','bold');
+uislider(yaxisBlock, 'range',...
+         'Limits',[min(nodes(:,1)), ceil(max(nodes(:,2)))],...
+         'Value', [min(nodes(:,1)), ceil(max(nodes(:,2)))],...
+         'Step', 5,...
+         'ValueChangedFcn',@(src,evt) updateY(ax1, ax2, src.Value, flags));
+
+%% --- Callback Functions ---
+    function updateFrame(ax1, ax2, s1, s2, framenum, colordata, flags)
+        frame = round(framenum);
+
+        % Update plot 1
+        s1.CData = real(colordata(:,flags.plot_volts,frame));
+        title(ax1, sprintf("Frame %d\nReal Voltage: CP %d", frame, flags.plot_volts))
+
+        % Update plot 2
+        if flags.set_complex
+            s2.CData = imag(colordata(:,flags.plot_volts,frame));
+            title(ax2, sprintf("Frame %d\nReal Voltage: CP %d", frame, flags.plot_volts))
+        end
+    end
+
+    function updateX(ax1, ax2, rangeVals, flags)
+        ax1.XLim = [rangeVals(1), rangeVals(2)];
+        if flags.set_complex
+            ax2.XLim = [rangeVals(1), rangeVals(2)];
+        end
+    end
+
+    function updateY(ax1, ax2, rangeVals, flags)
+        ax1.YLim = [rangeVals(1), rangeVals(2)];
+        if flags.set_complex
+            ax2.YLim = [rangeVals(1), rangeVals(2)];
+        end
+    end
